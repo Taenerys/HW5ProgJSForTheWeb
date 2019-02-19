b3e8f70c4ee240ad6ddb8af874b14b0062762093
Anh Pham <thuanh99@student.fdu.edu>
2019-02-18 18:23:04 -0800

    Add files via upload

---
+++ src/AddList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class AddList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {newList: []};
+  }
+
+  handleSubmit(e) {
+    e.preventDefault(); // this prevents the page from reloading -- do not delete this line!
+
+      // Implement the rest of this function here!
+      this.setState(
+        {newList: this.refs.id.value},
+        () => this.props.addList(this.state));
+      console.log(this.state.newList);
+  }
+
+  render() {
+    return (
+      <div id="addListDiv">
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div id='addList'>
+            <label>What will be on your next list?&nbsp;
+              <input type='text' ref='id' id='newID'></input>
+            </label>
+          </div><br />
+          <input type='submit' value='Create List' />
+          </form>
+        </div>
+      );
+    }
+  }
+
+export default AddList;
